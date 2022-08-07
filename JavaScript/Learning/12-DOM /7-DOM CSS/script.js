b3743fe5820b6f622ffe75abe8f0f770456431e5
giver-kdk <giverbro13@gmail.com>
2022-08-07 23:12:49 +0545

    Day 3

--- JavaScript/Learning/12-DOM /7-DOM CSS/script.js
+++ JavaScript/Learning/12-DOM /7-DOM CSS/script.js
@@ -9,4 +9,11 @@ element1.style.color = "red";
 element1.style.border = "2px solid blue";
 element1.style.borderRadius = "5px";
 element1.style.fontSize = "25px";
+
+// Using style just like in CSS with setProperty() method
+/*
+Syntax:
+	document.element.style.setProperty("property-name", "value");
+*/
+element1.style.setProperty("background-color", "palegreen");
 // There are more style property support: https://www.w3schools.com/jsref/dom_obj_style.asp
