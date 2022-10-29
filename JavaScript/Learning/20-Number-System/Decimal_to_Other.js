641d80fb1fa85c0c5b87c44cd9d3d3f4942eaf49
giver-kdk <giverbro13@gmail.com>
2022-10-29 11:09:12 +0545

    practice

---
+++ JavaScript/Learning/20-Number-System/Decimal_to_Other.js
@@ -0,0 +1,12 @@
+// By default, the numbers in JS are in decimal system. 
+// It can be converted to other system using "toString(base)" method, where "base" means radix
+
+
+let number = 12;								// Decimal Number
+console.log(number.toString(2));				// Conversion into Binary String
+console.log(number.toString(8));				// Conversion into Octal String 
+console.log(number.toString(16));				// Conversion into Hexadecimal String
+console.log(number.toString(10));				// Conversion into Decimal String(Same)
+
+// NOTE: The final result is in string form.
+
