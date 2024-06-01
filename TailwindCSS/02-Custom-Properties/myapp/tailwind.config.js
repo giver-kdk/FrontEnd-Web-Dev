eb5bf5b9babcbc28f2eceea773510e7b933fa952
Revig <giverbro13@gmail.com>
2024-06-01 23:26:00 +0545

    Custom Color Correction

--- TailwindCSS/02-Custom-Properties/myapp/tailwind.config.js
+++ TailwindCSS/02-Custom-Properties/myapp/tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-	  // Defining custom color theme 
-	  colors: {
-		"my-primary-color" : "#6DC5D1"
-	  },
 	  extend: {
+		// Defining custom color theme 
+		colors: {
+			"my-primary-color" : "#6DC5D1"
+		  },
 	  },
 	},
 	plugins: [],
